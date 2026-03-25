6386aa375200ed88c3c95834cb3542cfbda51c1e
SwethaAnand33 <159874560+SwethaAnand33@users.noreply.github.com>
2026-03-25 11:56:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
+import os
 from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # allow frontend to call backend
+CORS(app)
 
 @app.route('/welcome')
 def welcome():
     return jsonify({"message": "Welcome Master"})
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # get Render port
+    app.run(host='0.0.0.0', port=port)
