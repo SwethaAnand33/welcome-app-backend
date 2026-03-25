f4160342eecc87c651f61560e28d43e43dedd009
SwethaAnand33 <159874560+SwethaAnand33@users.noreply.github.com>
2026-03-25 12:22:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ CORS(app)
 def welcome():
     return jsonify({"message": "Welcome Master"})
 
+@app.route('/')
+def home():
+    return "Backend is running 🚀"
+
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # get Render port
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
